4bba44544719ca6ebe801075ecd82f5d398931f1
kittog <lena.gaubert@gmail.com>
2023-11-25 17:43:51 +0100

    nouvelle fonction tokenize_and_filter

--- scripts/vectorize.py
+++ scripts/vectorize.py
@@ -5,24 +5,30 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from tqdm import tqdm
+import re
 
-def process_txt(texts, lang):
+def tokenize_and_filter(texts, lang):
     lm = {
         "en":"en_core_web_sm",
         "it":"it_core_news_sm",
         "fr":"fr_core_news_sm"
     }
-    nlp = spacy.load(lm[lang])
-    stopwords = stopwordsiso.stopwords(lang)
-    clean_texts = []
-    for x in tqdm(texts):
-        clean_tokens = []
-        doc = nlp(x)
-        for token in doc:
-            if token not in stopwords:
-                clean_tokens.append(token.text)
-        clean_texts.append(" ".join(clean_tokens))
-    return clean_texts
+    nlp = spacy.load(lm[lang]) # language model
+    filtered_docs = []
+    for text in tqdm(texts):
+        doc = nlp(text)
+        # tokenize
+        tokens = [token.text for token in doc]
+        filtered_tokens = []
+        # filter out raw punctuation, numbers etc
+        for token in tokens:
+            if re.search('[a-zA-Z]', token):
+                filtered_tokens.append(token.lower())
+        # filter out stopwords
+        stop_words = stopwordsiso.stopwords(lang)
+        filtered_tokens = [token for token in filtered_tokens if token not in stop_words]
+        filtered_docs.append(" ".join(filtered_tokens)) # returns text, filtered
+    return filtered_docs
 
 
 def process_xml(input_xml, output_csv, output_tfidf_csv, lang, n_components=20):
@@ -38,10 +44,15 @@ def process_xml(input_xml, output_csv, output_tfidf_csv, lang, n_components=20):
             parties.append(party_value)
 
     df = pd.DataFrame({'Text': texts, 'Party': parties})
-    df["Clean"] = process_txt(texts, lang)
+    df['Clean'] = tokenize_and_filter(texts, lang)
     df.to_csv(output_csv, index=False)
 
-    tfidf_matrix_svd = TruncatedSVD(n_components=n_components).fit_transform(TfidfVectorizer().fit_transform(df["Clean"]))
+    tfidf_matrix_svd = TruncatedSVD(n_components=n_components).fit_transform(
+        TfidfVectorizer(
+            max_df=0.8,
+            min_df=0.1,
+        ).fit_transform(df['Clean']))
+    
     pd.DataFrame(tfidf_matrix_svd).to_csv(output_tfidf_csv, index=False)
 
 def process_all_languages():
